30431e7404a9d010deab70a6ed223edaf9371c24
promal7 <promalsharma@gmail.com>
2026-08-01 16:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.35%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uni(int i, int j, int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=uni(i-1, j, dp);
+        int left=uni(i, j-1, dp);
+        return dp[i][j]=up+left;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return uni(m-1, n-1, dp);
+    }
+}
