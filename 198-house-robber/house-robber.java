b69301f5e32dce0abc357d6dda229d1e5283fa31
promal7 <promalsharma@gmail.com>
2026-08-29 10:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.86%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,17 @@
 class Solution {
+    public int house(int ind, int[] nums, int[] dp){
+        if(ind<0) return 0;
+        if(ind==0) return nums[ind];
+        if(dp[ind]!=-1) return dp[ind];
+        int notpick=house(ind-1, nums, dp);
+        int pick=nums[ind]+house(ind-2, nums, dp);
+        return dp[ind]=Math.max(pick, notpick);
+    }
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n==1) return nums[0];
         int[] dp=new int[n];
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for(int i=2; i<n; i++){
-            dp[i]=Math.max(nums[i]+dp[i-2], dp[i-1]);
-        }
-        return dp[n-1];
+        Arrays.fill(dp, -1);
+        return house(n-1, nums, dp);
+
     }
 }
