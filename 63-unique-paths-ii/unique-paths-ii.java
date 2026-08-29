02568b90ecadbb311bbb35b4d99bd215a5870c96
promal7 <promalsharma@gmail.com>
2026-08-29 11:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (81.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int uniqpath(int i, int j, int[][] obstacleGrid, int[][] dp){
-        if(i<0 || j<0 || obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=uniqpath(i-1, j, obstacleGrid, dp);
-        int up=uniqpath(i,j-1, obstacleGrid, dp);
-        return dp[i][j]=left+up;
+    public int uniqpath(int ind1, int ind2, int[][] obstacleGrid, int[][] dp){
+        if(ind1<0 || ind2<0 || obstacleGrid[ind1][ind2]==1) return 0;
+        if(ind1==0 && ind2==0) return 1;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        int left=uniqpath(ind1, ind2-1, obstacleGrid, dp);
+        int up=uniqpath(ind1-1, ind2, obstacleGrid, dp);
+        return dp[ind1][ind2]=left+up;
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n=obstacleGrid.length;
