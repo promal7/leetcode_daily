5e4a4634a8b5161b1ceed6b29299a48f0806e674
promal7 <promalsharma@gmail.com>
2026-09-02 13:16:42 +0530

    Time: 1 ms (69.16%) | Memory: 43.6 MB (47.62%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxp(int i, int buy, int[] prices, int[][] dp){
+        if(i>=prices.length) return 0;
+        int profit=0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(buy==0){
+            profit=Math.max(-prices[i]+maxp(i+1, 1, prices, dp), maxp(i+1, 0, prices, dp));
+        }else{
+            profit=Math.max(prices[i]+maxp(i+2, 0, prices, dp), maxp(i+1, 1, prices, dp));
+        }
+        return dp[i][buy]=profit;
+    }
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][] dp=new int[n][2];
+        for(int[] row: dp) Arrays.fill(row, -1);
+        return maxp(0, 0, prices, dp);
+    }
+}
