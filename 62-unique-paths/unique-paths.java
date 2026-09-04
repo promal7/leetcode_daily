eec72c51f40a94674e6ef7cf2c36b5bfabd14a69
promal7 <promalsharma@gmail.com>
2026-09-04 22:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.87%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int uniqpath(int ind1, int ind2, int[][] dp){
-        if(ind1<0 || ind2<0) return 0;
-        if(ind1==0 && ind2==0) return 1;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        int left=uniqpath(ind1, ind2-1, dp);
-        int up=uniqpath(ind1-1, ind2, dp);
-        return dp[ind1][ind2]=left+up;
+    public int uniq(int i, int j, int m, int n, int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=uniq(i-1, j, m, n, dp);
+        int left=uniq(i, j-1, m, n, dp);
+        return dp[i][j]=up+left;
     }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
-        }
-        return uniqpath(m-1, n-1, dp);
+        for(int[] row: dp) Arrays.fill(row, -1);
+        return uniq(m-1, n-1, m, n, dp);
     }
 }
