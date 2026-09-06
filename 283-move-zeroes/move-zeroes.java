b32caaffe81c25f9062d94f8079f7d7605601a78
promal7 <promalsharma@gmail.com>
2026-09-06 19:23:28 +0530

    Time: 2 ms (91.78%) | Memory: 47.5 MB (93.21%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int l=0;
+        for(int r=0; r<n; r++){
+            if(nums[r]!=0){
+                int temp=nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
+            }
+        }
+    }
+}
